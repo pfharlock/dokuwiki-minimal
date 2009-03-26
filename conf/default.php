f2c87872c2d53c170b99925696d5a39f928f56fa
François Hodierne <francois@upian.com>
2009-03-26 16:46:23 +0100

    remove LD in default footer

--- conf/default.php
+++ conf/default.php
@@ -4,4 +4,4 @@ $conf['width'] = 'auto';
 $conf['font-size'] = '1.1em';
 $conf['title-color'] = '#009DFF';
 $conf['hide-entry-title'] = false;
-$conf['footer-text'] = 'Powered by <a href="http://dokuwiki.org/">DokuWiki</a> with a touch of <a href="http://h6e.net/">h6e</a> via <a href="http://ladistribution.h6e.net/">La Distribution</a>';
+$conf['footer-text'] = 'Powered by <a href="http://dokuwiki.org/">DokuWiki</a> with a touch of <a href="http://h6e.net/">h6e</a>';
