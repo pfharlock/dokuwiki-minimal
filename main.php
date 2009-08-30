93db3ab3d048d85e3f23f8af0b478c12db741647
Gary Watson <watsong@nku.edu>
2009-08-30 11:38:23 -0400

    putting my own changes into the template for my website.
    Basically I've
      * re-arranged some things on the page
      * Added a few elements that were missing
      * put my own breadcrumb stuff at the top (not generic)
      * changed the template so it fills the entire screen
      * added indenting into the template so Headings look right
      * put in a watermark image that matches the theme of my website
      * removed advertisements for dokuwiki and h6e
      * put backlink functionality into the title because I'm used to it
      * added the ability to display section subscriptions as well as page subscriptions
    
    Potential todo items at this point
      * remove the margin-left from the breadcrumbs, it looks goofy moved over the way it currently is

--- main.php
+++ main.php
@@ -27,16 +27,15 @@ if (empty($_REQUEST['do']) || in_array($_REQUEST['do'], array('revisions', 'show
 
   <link rel="stylesheet" media="screen" href="<?php echo H6E_CSS ?>/h6e-minimal/h6e-minimal.css" />
 
-  <?php tpl_metaheaders() ?>
+  <?php tpl_metaheaders()?>
   
   <style type="text/css">
-  <?php if (tpl_getConf('width') != 'auto') : ?>
+
   .h6e-main-content {
       width:<?php echo tpl_getConf('width') ?>;
       padding-left:2.5em;
       padding-right:2.5em;
   }
-  <?php endif ?>
   .h6e-post-content {
       font-size:<?php echo tpl_getConf('font-size') ?>;
   }
@@ -51,7 +50,7 @@ if (empty($_REQUEST['do']) || in_array($_REQUEST['do'], array('revisions', 'show
 
 <div class="dokuwiki">
 
-  <?php html_msgarea() ?>
+  <?php html_msgarea()?>
 
   <?php
   if (tpl_getConf('topbar') == 'never') {
@@ -66,24 +65,34 @@ if (empty($_REQUEST['do']) || in_array($_REQUEST['do'], array('revisions', 'show
   <?php if (class_exists('Ld_Ui') && method_exists('Ld_Ui', 'top_bar') && $top_bar) : ?>
      <?php Ld_Ui::top_bar() ?>
   <?php else : ?>
-    <div class="user-info">
+      <div class="user-info" style="float:right">
         <?php tpl_userinfo()?>
-        <?php tpl_actionlink('subscription') ?>
+        <?php tpl_actionlink('subscribe') ?>
+        <?php tpl_actionlink('subscribens') ?>
+
         <?php tpl_actionlink('profile') ?>
         <?php tpl_actionlink('admin') ?>
         <?php tpl_actionlink('login'); ?>
-    </div>
+      </div>
   <?php endif ?>
+
+  <div style="padding-top: 10px; padding-left: 30px;">
+  <h3><a href="/">The Singularity</a> &gt; <?php tpl_link(wl(),$conf['title'],'name="dokuwiki__top" id="dokuwiki__top" accesskey="h" title="[ALT+H]"') ?> &gt; <?php tpl_actionlink('backlink')?> </h3>
+  </div>
 
   <div class="<?php echo $page_type ?> h6e-main-content">
 
-    <h1 class="h6e-page-title">
-    <?php tpl_link(wl(),$conf['title'],'name="dokuwiki__top" id="dokuwiki__top" accesskey="h" title="[ALT+H]"') ?>
-    </h1>
+    <div style="float:right">
+    <?php tpl_searchform() ?>
+    </div>
+    <div class="actions actions-page">
+        <?php tpl_button('edit')?>
+        <?php tpl_button('history')?>
+    </div>
 
     <?php if (!tpl_getConf('hide-entry-title')){?>
         <h2 class="h6e-entry-title">
-        <?php tpl_pagetitle($ID) ?>
+        [[<?php tpl_link(wl($ID,'do=backlink'),tpl_pagetitle($ID,true),'title="'.$lang['btn_backlink'].'"')?>]]
         </h2>
     <?php }?>
 
@@ -107,21 +116,12 @@ if (empty($_REQUEST['do']) || in_array($_REQUEST['do'], array('revisions', 'show
         <?php tpl_pageinfo()?>
     </div>
 
-    <div class="actions actions-page">
-        <?php tpl_button('edit')?>
-        <?php tpl_button('history')?>
-        <?php tpl_button('backlink')?>
-    </div>
-
     <div class="h6e-simple-footer">
 
       <div class="actions actions-site">
           <div class="a">
               <?php tpl_button('recent')?>
               <?php tpl_button('index')?>
-          </div>
-          <div class="b">
-              <?php tpl_searchform() ?>
           </div>
       </div>
 
