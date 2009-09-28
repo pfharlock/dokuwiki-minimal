28024691b250548ca58f523892b40c6a14c43a67
François Hodierne <francois@upian.com>
2009-09-28 17:06:29 +0200

    mutualize top.php and pass login/logout urls to top_bar function

--- main.php
+++ main.php
@@ -51,29 +51,7 @@ if (empty($_REQUEST['do']) || in_array($_REQUEST['do'], array('revisions', 'show
 
 <div class="dokuwiki">
 
-  <?php html_msgarea() ?>
-
-  <?php
-  if (tpl_getConf('topbar') == 'never') {
-      $top_bar = false;
-  } else if (tpl_getConf('topbar') == 'connected' && empty($_SERVER['REMOTE_USER'])) {
-      $top_bar = false;
-  } else {
-      $top_bar = true;
-  }
-  ?>
-
-  <?php if (class_exists('Ld_Ui') && method_exists('Ld_Ui', 'top_bar') && $top_bar) : ?>
-     <?php Ld_Ui::top_bar() ?>
-  <?php else : ?>
-    <div class="user-info">
-        <?php tpl_userinfo()?>
-        <?php tpl_actionlink('subscription') ?>
-        <?php tpl_actionlink('profile') ?>
-        <?php tpl_actionlink('admin') ?>
-        <?php tpl_actionlink('login'); ?>
-    </div>
-  <?php endif ?>
+  <?php include dirname(__FILE__) . '/top.php' ?>
 
   <div class="<?php echo $page_type ?> h6e-main-content">
 
